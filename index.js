f806c7c6215316dec2f786d70aa86cffb70eeee2
Lucija <lucija.strejcek@fer.hr>
2022-01-25 16:46:53 +0100

    Add native api

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+//app to understand incoming data as JSON
+app.use(express.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/camera", (req, res) => {
+  res.render("camera");
 });
 
 app.listen(port, () => {
